83007069010019682259d7ff46ffab6bb3c5f970
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-15 00:23:39 +0300

    add style to nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
                   <img src={appleImg} alt="Apple" width={14} height={18} />
                   <div className="flex flex-1 justify-center max-sm:hidden ">
                       {navLists.map((item) => (
-                      <div key={item}>
+                      <div key={item} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
                             {item}
                     </div>  
                 ))}
