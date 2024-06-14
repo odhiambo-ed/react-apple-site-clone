2be11b62817b80bc313b8fab75cb12cd47b1c7f1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-15 00:07:07 +0300

    add nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,26 @@
+import { appleImg, bagImg, searchImg } from "../utils";
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
+    <>
+      <header>
+        <nav>
+                  <img src={appleImg} alt="Apple" width={14} height={18} />
+                  <div>
+                      {["Phone", "Mac", "iPad", "Watch", "TV", "Music", "Support", "Where to Buy"].map((item) => (
+                      <div key={item}>
+                            {item}
+                    </div>  
+                ))}
+                  </div>
+                  <div>
+                      <img src={searchImg} alt="Search" width={18} height={18} />
+                      <img src={bagImg} alt="Bag" width={18} height={18} />
+                  </div>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default NavBar
