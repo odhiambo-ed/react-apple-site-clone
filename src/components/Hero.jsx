da35a8ac8cde1ae6dc64080f73d45e6bd41ca2f6
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-15 20:41:40 +0300

    add useGSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to('#hero', {opacity: 1, delay: 1}
+    )  },[])
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <div className="hero-title">iPhone 15 pro</div>
+        <div id='hero' className="hero-title">iPhone 15 pro</div>
       </div>
     </section>
   )
